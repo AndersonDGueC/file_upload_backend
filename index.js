a7c81bea762c4ca38831b6f7653aa87bce8c62e1
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-11-18 18:48:41 -0500

    finally proyect upload file

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/fileanalyse",(req,res)=>{
-  res.json({message:"hi hacker"})
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res)=>{
+  res.json({name:req.file.originalname,
+            type:req.file.mimetype,
+            size:req.file.size
+  })
 })
 
 
